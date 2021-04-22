2a6e2a785bb2424b45d2b9d2069770596af3dee8
pomubry <btaduran30@protonmail.com>
2021-04-22 20:50:30 +0800

    removed filtering in bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,11 +1,9 @@
 (function () {
   let arr = document.getElementsByTagName("img");
   let copy = [...arr];
-  let filtered = copy
-    .filter((img) => img.src.includes("download/file.php?id"))
-    .map((img) => ({
-      src: img.src.replace("&t=1", ""),
-      alt: img.alt,
-    }));
-  console.log(filtered);
+  let sources = copy.map((img) => ({
+    src: img.src,
+    alt: img.alt,
+  }));
+  console.log(sources);
 })();
